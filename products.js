fc4fd868c7063a25f930342d955c56b50d5faded
CarterMoreMoney <barshiruwaris33@gmail.com>
2025-07-21 15:13:07 +0000

    Update products.js

--- products.js
+++ products.js
@@ -1,1 +1,44 @@
-
+const products = [
+  {
+    title: "Hoodie",
+    image: "hoodie.webp",
+    fallback: "hoodie.JPG",
+    link: "product-hoodie.html"
+  },
+  {
+    title: "Thrift",
+    image: "thrift.webp",
+    fallback: "thrift.JPG",
+    link: "product-thrift.html"
+  },
+  {
+    title: "Spectacles",
+    image: "spectacles.webp",
+    fallback: "spectacles.JPG",
+    link: "product-spectacles.html"
+  },
+  {
+    title: "Designer Bags",
+    image: "designer-bags.webp",
+    fallback: "designer-bags.JPG",
+    link: "product-designer-bags.html"
+  },
+  {
+    title: "Jewelries",
+    image: "jewelries.webp",
+    fallback: "jewelries.JPG",
+    link: "product-jewelries.html"
+  },
+  {
+    title: "Designer Socks",
+    image: "designer-socks.webp",
+    fallback: "designer-socks.JPG",
+    link: "product-designer-socks.html"
+  },
+  {
+    title: "Baseball Caps",
+    image: "baseball-caps.webp",
+    fallback: "baseball-caps.JPG",
+    link: "product-baseball-caps.html"
+  }
+];
